dab2052053b1f6ce87defcb4d71ed951a14193ea
Hyunwoo <hw5773@gmail.com>
2020-06-01 07:45:01 +0900

    add resources

--- src/FogProxyInterface.java
+++ src/FogProxyInterface.java
@@ -1,7 +1,7 @@
-import FlexID;
+import FlexID.*;
 
 public interface FogProxyInterface {
 
     // Initialize a resource of interest and register it (device / service) to FogOS.
-    public FlexID
+
 }
