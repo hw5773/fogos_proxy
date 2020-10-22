d8e4bb1616ee10e656d2ea1d6e49f07f6e243d08
hmlee <min0921110@gmail.com>
2020-10-22 12:52:25 +0100

    modify

--- src/FogProxyCloud.java
+++ src/FogProxyCloud.java
@@ -4,6 +4,7 @@ import FogOSClient.FogOSClient;
 import FogOSContent.Content;
 import FogOSResource.Resource;
 import FogOSResource.ResourceType;
+import FogOSSecurity.SecureFlexIDSession;
 import FogOSService.Service;
 import FogOSService.ServiceContext;
 import FogOSService.ServiceType;
@@ -13,6 +14,7 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -24,8 +26,9 @@ public class FogProxyCloud {
 	
 	private static final String key = "key";
 	private static final String host = "ip address";
-	
-	private static final String rootPath = "D:\\tmp";
+
+	//private static final String rootPath = "D:\\tmp";
+	private static final String rootPath = "C:\\Users\\HMLEE\\FogOS";
     
     public static void main(String[] args) throws NoSuchAlgorithmException, InterruptedException, IOException {
     	// 1. Initialize the FogOSClient instance.
@@ -35,6 +38,7 @@ public class FogProxyCloud {
 
         // 2. Add manually resource, content, or service
 		// 2-1. Add resources to be monitored
+		/*
 		Resource resource_cpu = new Resource("CPU", ResourceType.CPU, "","percent",false) {
 			@Override
 			public void monitorResource() {
@@ -70,22 +74,25 @@ public class FogProxyCloud {
 		fogos.addResource(resource_cpu);
 		fogos.addResource(resource_disk);
 		fogos.addResource(resource_mem);
+		*/
 
 		// 2-2. Add content manually if any
-		Content test_content = new Content("test_content", "D:\\tmp\\test.png", true);
+		//Content test_content = new Content("test_content", "D:\\tmp\\test.png", true);
+		Content test_content = new Content("test_content", "C:\\Users\\HMLEE\\FogOS\\test.txt", true);
 		fogos.addContent(test_content);
+
 
 		// 2-3. Add service to run
 		KeyPairGenerator proxyKeyPairGenerator = KeyPairGenerator.getInstance("RSA");
 		proxyKeyPairGenerator.initialize(2048);
 		KeyPair proxyKeyPair = proxyKeyPairGenerator.genKeyPair();
-		Locator proxyLoc = new Locator(InterfaceType.WIFI, "127.0.0.1", 5550);
+		Locator proxyLoc = new Locator(InterfaceType.WIFI, "127.0.0.1", 5551);
 		Locator serverLoc = new Locator(InterfaceType.ETH, "52.78.23.173", 80);
 		ServiceContext testServiceCtx1 = new ServiceContext("FogClientTestService",
 				ServiceType.Streaming, proxyKeyPair, proxyLoc, true, serverLoc);
 		Service testService1 = new Service(testServiceCtx1) {
 			@Override
-			public void initService() throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException, IOException {
+			public void initService() throws Exception {
 				super.initService();
 			}
 
@@ -95,7 +102,10 @@ public class FogProxyCloud {
 				byte[] buf = new byte[16384];
 				ByteBuffer input = this.getInputFromServer(buf);
 				// Send data to Peer
+				//System.out.println("!!!!!!!!!!!!!!!!!!!!!");
+				//System.out.println(new String(input.array()).trim());
 				this.putOutputToPeer(input);
+				input.clear();
 			}
 
 			@Override
@@ -110,6 +120,7 @@ public class FogProxyCloud {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				output.clear();
 			}
 
 			@Override
@@ -118,6 +129,7 @@ public class FogProxyCloud {
 				ByteBuffer input = this.getInputFromPeer(buf);
 				if (this.getContext().isProxy())
 					this.putOutputToServer(input);
+				input.clear();
 			}
 
 			@Override
@@ -127,15 +139,30 @@ public class FogProxyCloud {
 				ByteBuffer output = this.getOutputToPeer(buf);
 				// Send Data to Peer
 				byte[] outputBuf = output.array();
-				this.getPeerSession().send(outputBuf, outputBuf.length);
+				System.out.println("HBBBBBBBBBBBBBBBBBBBBBB");
+				System.out.println(new String(outputBuf).trim());
+				SecureFlexIDSession secureFlexIDSession = getPeerSession();
+				secureFlexIDSession.send(new String(outputBuf).trim());
+				output.clear();
 			}
 		};
 		fogos.addService(testService1);
+
         
         // 3. begin the FogOS interaction
 		fogos.begin();
 		System.out.println("[FogProxyCloud] FogOS Core begins.");
 
+		// Explicitly register content
+		HashMap<String, String> attributes = new HashMap<String, String>();
+		attributes.put("title", "TestContent");
+		attributes.put("desc", "Test");
+		fogos.registerContent(test_content, attributes);
+		//fogos.registerContent("test.jpg", "D:\\tmp\\test.jpg");
+
+		// Explicitly register service
+
+		fogos.registerService(testService1);
 //		 4. finalize the FogOS interaction
 		fogos.exit();
 		System.out.println("[FogProxyCloud] FogOS Core quits.");
